149c1d5f0564c61b74081499575c66b769e1b6ed
ykis-0-0 <64165725+ykis-0-0@users.noreply.github.com>
2020-07-06 20:45:16 +0800

    Added logging facilities

---
+++ src/utils/logging/handler.ts
@@ -0,0 +1,18 @@
+import { BaseHandler } from 'deno_std/log/handlers.ts';
+import { LogRecord } from 'deno_std/log/logger.ts';
+import { LevelName } from 'deno_std/log/levels.ts'
+
+export default class MyConsoleHandler extends BaseHandler {
+
+  constructor(lvl: LevelName, options: object){
+    super(lvl, options);
+  }
+
+  format(logRecord: LogRecord): string {
+    return super.format(logRecord);
+  }
+
+  log(msg: string): void {
+    console.log(msg);
+  }
+}
---
+++ src/utils/logging/mod.ts
@@ -0,0 +1,78 @@
+import * as log from 'deno_std/log/mod.ts';
+import {Logger} from 'deno_std/log/logger.ts';
+
+import getDateString from '../dstring_iso.ts';
+import MyConsoleHandler from './handler.ts';
+import formatter from './formatter.ts';
+
+let date: string = getDateString(true);
+
+let config: log.LogConfig = {
+  handlers: {
+    console: new MyConsoleHandler('DEBUG', {
+      formatter: formatter(true)
+    }),
+    file: new log.handlers.FileHandler('DEBUG', {
+      filename: `./logs/${date}/default.log`,
+      mode: 'x',
+      formatter: formatter(false)
+    }),
+    file_http: new log.handlers.FileHandler('DEBUG', {
+      filename: `./logs/${date}/http_server.log`,
+      mode: 'x',
+      formatter: formatter(false)
+    }),
+    file_api: new log.handlers.FileHandler('DEBUG', {
+      filename: `./logs/${date}/apis.log`,
+      mode: 'x',
+      formatter: formatter(false)
+    }),
+    fatal: new log.handlers.FileHandler('WARNING', {
+      filename: './error.log',
+      mode: 'a',
+      formatter: formatter(false)
+    })
+  },
+  loggers: {
+    default: {
+      level: 'DEBUG',
+      handlers: ['console', 'file', 'fatal']
+    },
+    http_server: {
+      level: 'DEBUG',
+      handlers: ['console', 'file_http_server', 'fatal']
+    },
+    apis: {
+      level: 'DEBUG',
+      handlers: ['console', 'file_apis', 'fatal']
+    },
+    console: {
+      level: 'DEBUG',
+      handlers: ['console']
+    },
+    file: {
+      level: 'DEBUG',
+      handlers: ['file']
+    }
+  }
+}
+
+await Deno.mkdir(`./logs/${date}`, {recursive: true});
+
+await log.setup(config);
+
+export default function register(logger: string, moduleName: string | null = null) : Logger{
+  if(!(1 in arguments)) {
+    return log.getLogger(logger);
+  }
+  return new Proxy(log.getLogger(logger), {
+    get: function(t: Logger, p: string, r) {
+      if(!['debug', 'info', 'warning', 'error', 'critical'].includes(p)) {
+        return Reflect.get(t, p, r)
+      }
+      return function(msg: unknown, ...args: unknown[]) {
+        Reflect.apply(t[p as 'debug' | 'info' | 'warning' | 'error' | 'critical'], t, [msg, moduleName].concat(args))
+      }
+    }
+  })
+}
