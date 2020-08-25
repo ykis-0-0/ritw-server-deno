46a67f978e84982f44958fc488e2c079e0ecc315
ykis-0-0 <64165725+ykis-0-0@users.noreply.github.com>
2020-08-25 22:59:07 +0800

    Fix batch

--- start.bat
+++ start.bat
@@ -1,9 +1,5 @@
 @echo off
 SETLOCAL
 SET DENO_DIR=%~dp0\deno_modules
-IF "%VSCODE%"=="VSCODE" (
-  START %~dp0\deno.exe %*
-) ELSE (
-  %~dp0\deno.exe %*
-  PAUSE > NUL
-)
+%~dp0\deno.exe %*
+IF NOT "%VSCODE%"=="VSCODE" PAUSE > NUL
