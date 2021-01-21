1248cf66c068460b3ae529b132c179758f0791e8
ykis-0-0 <64165725+ykis-0-0@users.noreply.github.com>
2021-01-21 12:21:09 +0800

    Minor Renaming

--- src/utils/logging/formatter.ts
+++ src/utils/logging/formatter.ts
@@ -11,9 +11,10 @@ export default (isConsole: boolean) => function formatter(_: LogRecord): string 
     ifelse // Expressionized If-else condition
       : <I, O>(_: ($: any) => boolean) => (t: (_: I) => O, f: (_: I) => O) => ($: any) => ((_: I) => O)
       = _ => (t, f) => $ => _($) ? t : f,
-    cases // Expressionized `switch` clause for Functions typed (In => Out)
+    fnCases // Expressionized `switch` clause for Functions typed (In => Out)
       : <Mi, M, F, T>(f: (_: Mi) => M, m: M[], t: ((_: F) => T)[], d:((_: F) => T)) => ($: Mi) => ((_: F) => T)
       = (f, m, t, d) => $ => (<F, T>(_: ((_: F) => T)[]) => _.length === 1 ? _[0] : d)(t.filter((_, i) => m[i] === f($)));
+
   let
     wrap // in [brackets]
       : (_: string) => string
@@ -21,16 +22,17 @@ export default (isConsole: boolean) => function formatter(_: LogRecord): string 
     forConsole // or we turn off colorings
       = ($: ($$: string) => string) => ifelse<string, string>(iden)($, iden)(isConsole),
     choose // formattings from level
-      = cases<string, string, string, string>(iden, ['CRITICAL', 'ERROR', 'WARNING', 'INFO', 'DEBUG'], [_ => colors.bold(colors.red(_)), colors.red, colors.yellow, colors.dim, iden], iden),
+      = fnCases<string, string, string, string>(iden, ['CRITICAL', 'ERROR', 'WARNING', 'INFO', 'DEBUG'], [_ => colors.bold(colors.red(_)), colors.red, colors.yellow, colors.dim, iden], iden),
     highlight // Ummmm.. What's this?
       = ifelse<string, string>(_ => _ === 'CRITICAL')(colors.red, iden);
+
   let
-    t: string = getDateString(undefined, _.datetime),
-    l: string = ifelse<string, string>(iden)((_: string) => choose(_)(_), iden)(isConsole)(_.levelName), // We chose to only color the level names, this way it looks cleaner
-    n: string = _.loggerName,
-    e: string[] | undefined = 0 in _.args ? [_.args[0] as string] : undefined;
+    time: string = getDateString(undefined, _.datetime),
+    lvel: string = ifelse<string, string>(iden)((_: string) => choose(_)(_), iden)(isConsole)(_.levelName), // We chose to only color the level names, this way it looks cleaner
+    name: string = _.loggerName,
+    path: string[] | undefined = 0 in _.args ? [_.args[0] as string] : undefined;
   let r: string =
-    '    ' + forConsole(colors.white)(Array.prototype.concat.apply([t, l, n], e as any[]).map(wrap).join(' ')) + '\n'+
+    '    ' + forConsole(colors.white)(Array.prototype.concat.apply([time, lvel, name], path as any[]).map(wrap).join(' ')) + '\n'+
     _.msg +
     '\n';
   return r;
