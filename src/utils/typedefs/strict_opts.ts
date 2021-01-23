eb5bb06e4567992e08fdd8785a78429b7e0c07a1
ykis-0-0 <64165725+ykis-0-0@users.noreply.github.com>
2021-01-23 08:26:17 +0800

    Reconstructed intermediate types
    - OptPropNames<>: Changed from Record<> to mapped types for much better clarity
    - OptPropNames<>: Exclude<> now not needed after ditching Record<> and adding `-?` to outer map
    - Optionals<>: Required<O>[_] is much clearer than Exclude<{...}> with same effect
    - Extracted processing of required properties to NonOptionals<>

--- src/utils/typedefs/strict_opts.ts
+++ src/utils/typedefs/strict_opts.ts
@@ -1,21 +1,24 @@
 import * as Ops from './type_ops.ts';
 
-type OptPropNames<O extends object> = Exclude<{
-  [K in keyof O]: O extends Record<K, O[K]> ? never : K;
-}[keyof O], undefined>
+type OptPropNames<O extends object> = {
+  [K in keyof O]-?: O extends {[_ in K]-?: O[_]} ? never : K;
+}[keyof O];
 
 // Wrapped union in {_: T} to prevent early merging of branches
 type Optionals<O extends object> = Ops.UnionToIntersection<{
-  [K in OptPropNames<O>]-?: {
-    _: {
-      [_ in K]: Exclude<O[_] extends object ? StrictOptional<O[_]> : O[_], undefined>;
-    } | {}
+  [K in OptPropNames<O>]: {
+    _: {[_ in K]: Required<O>[_] extends object ? StrictOptional<Required<O>[_]> : Required<O>[_];}
+     | {}
   };
 }[OptPropNames<O>]>;
 
+type NonOptionals<O extends object> = {
+  _: {
+    [K in Exclude<keyof O, OptPropNames<O>>]: O[K] extends object ? StrictOptional<O[K]> : O[K];
+  };
+};
+
 // Will have error if we don't index it as a whole, dunno why tho
-type StrictOptional<O extends object> = ({_: {
-  [K in Exclude<keyof O, OptPropNames<O>>]: O[K] extends object ? StrictOptional<O[K]> : O[K];
-}} & Optionals<O>)['_'];
+type StrictOptional<O extends object> = (NonOptionals<O> & Optionals<O>)['_'];
 
 export default StrictOptional;
