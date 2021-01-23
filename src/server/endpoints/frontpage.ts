cd8c8cd59336fd3763bf9154837aab94b1f3f654
ykis-0-0 <64165725+ykis-0-0@users.noreply.github.com>
2021-01-23 09:10:14 +0800

    Initial partial implementation (I don't think it works)

---
+++ src/server/endpoints/frontpage.ts
@@ -0,0 +1,14 @@
+import type * as path from 'std://path/mod.ts';
+
+// @deno-types='https://github.com/DefinitelyTyped/DefinitelyTyped/raw/master/types/mustache/index.d.ts'
+import mustache from 'https://github.com/janl/mustache.js/raw/master/mustache.mjs';
+
+import type { DispatcherRequest } from '../dispatcher.ts';
+
+class MustacheBuilder {
+  constructor(dir: path.ParsedPath){
+    // TODO
+  }
+}
+
+export default (req: DispatcherRequest) => undefined;
--- src/server/mod.ts
+++ src/server/mod.ts
@@ -1,5 +1,11 @@
 import Dispatcher from './dispatcher.ts';
 
+import frontsiteHnd from './endpoints/frontpage.ts';
+
 const dispatchRoot = new Dispatcher();
+
+//const fronsiteHnd = new FrontSiteBuilder()
+
+dispatchRoot.registerTrigger('/site', frontsiteHnd);
 
 export default dispatchRoot;
