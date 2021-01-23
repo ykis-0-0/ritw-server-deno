ab1f64106dcb8cd2d63520d217a26f7f588a34cf
ykis-0-0 <64165725+ykis-0-0@users.noreply.github.com>
2021-01-23 08:44:43 +0800

    Adapt towards edge cases
    Works to eliminate `{}`, which allows appearance of `undefined`
    - Optionals: Replaced `{}` with `{[_]: never}` to forbid `undefined`
    - NonOptionals: if all props optional emit unknown instead of letting it resolves to {}, since `unknown & T === T`

--- src/utils/typedefs/strict_opts.ts
+++ src/utils/typedefs/strict_opts.ts
@@ -8,17 +8,18 @@ type OptPropNames<O extends object> = {
 type Optionals<O extends object> = Ops.UnionToIntersection<{
   [K in OptPropNames<O>]: {
     _: {[_ in K]: Required<O>[_] extends object ? StrictOptional<Required<O>[_]> : Required<O>[_];}
-     | {}
+     | {[_ in K]: never;};
   };
 }[OptPropNames<O>]>;
 
-type NonOptionals<O extends object> = {
-  _: {
-    [K in Exclude<keyof O, OptPropNames<O>>]: O[K] extends object ? StrictOptional<O[K]> : O[K];
-  };
-};
+type NonOptionals<O extends object>
+   = keyof O extends OptPropNames<O>
+   ? {_: unknown} // Used `unknown` to prevent the `{}` from appearing
+   : {_: {[K in Exclude<keyof O, OptPropNames<O>>]: O[K] extends object ? StrictOptional<O[K]> : O[K];};};
 
-// Will have error if we don't index it as a whole, dunno why tho
+// Will have error if we don't wrap and index it as a whole, dunno why tho
+// Seems we still can't unwrap it in an earlier phase due to some mystic error
+// Looks like it's related to the early map and indexing of Optionals<>?
 type StrictOptional<O extends object> = (NonOptionals<O> & Optionals<O>)['_'];
 
 export default StrictOptional;
