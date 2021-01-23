7288211d5e3f2658ef0543ab65201535927d91bd
ykis-0-0 <64165725+ykis-0-0@users.noreply.github.com>
2021-01-23 08:17:20 +0800

    Preliminary Renaming
    why the fuck did i make the name *that* long?

--- src/utils/typedefs/strict_opts.ts
+++ src/utils/typedefs/strict_opts.ts
@@ -1,22 +1,21 @@
 import * as Ops from './type_ops.ts';
 
-type OptionalPropNames<O extends object> = Exclude<{
+type OptPropNames<O extends object> = Exclude<{
   [K in keyof O]: O extends Record<K, O[K]> ? never : K;
 }[keyof O], undefined>
 
-
-// Wrap union in object to prevent early merging
-type OptionalCollection<O extends object> = Ops.UnionToIntersection<{
-  [K in OptionalPropNames<O>]-?: {
+// Wrapped union in {_: T} to prevent early merging of branches
+type Optionals<O extends object> = Ops.UnionToIntersection<{
+  [K in OptPropNames<O>]-?: {
     _: {
-      [K2 in K]: Exclude<O[K2] extends object ? StrictOptional<O[K2]> : O[K2], undefined>;
+      [_ in K]: Exclude<O[_] extends object ? StrictOptional<O[_]> : O[_], undefined>;
     } | {}
   };
-}[OptionalPropNames<O>]>;
+}[OptPropNames<O>]>;
 
 // Will have error if we don't index it as a whole, dunno why tho
 type StrictOptional<O extends object> = ({_: {
-  [K in Exclude<keyof O, OptionalPropNames<O>>]: O[K] extends object ? StrictOptional<O[K]> : O[K];
-}} & OptionalCollection<O>)['_'];
+  [K in Exclude<keyof O, OptPropNames<O>>]: O[K] extends object ? StrictOptional<O[K]> : O[K];
+}} & Optionals<O>)['_'];
 
 export default StrictOptional;
