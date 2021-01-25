09a345ade3f4db3f2796cc7bfd70fd7d0546f36b
ykis-0-0 <64165725+ykis-0-0@users.noreply.github.com>
2021-01-26 00:21:21 +0800

    A (seems to be) complete impl of StrictOptional
    , with `undefined` properly handled

--- src/utils/typedefs/strict_opts.ts
+++ src/utils/typedefs/strict_opts.ts
@@ -4,10 +4,15 @@ type OptPropNames<O extends object> = {
   [K in keyof O]-?: O extends {[_ in K]-?: O[_]} ? never : K;
 }[keyof O];
 
+type OptWithUndef<O extends object, K extends keyof O>
+  = O[K] extends undefined
+  ? O[K] extends never ? never : undefined
+  : Required<O>[K] extends object ? StrictOptional<Required<O>[K]> : Required<O>[K];
+
 // Wrapped union in {_: T} to prevent early merging of branches
 type Optionals<O extends object> = Ops.UnionToIntersection<{
   [K in OptPropNames<O>]: {
-    _: {[_ in K]: Required<O>[_] extends object ? StrictOptional<Required<O>[_]> : Required<O>[_];}
+    _: {[_ in K]: OptWithUndef<O, _>;}
      | {[_ in K]: never;};
   };
 }[OptPropNames<O>]>;
