5e9bcfd911c5ef26b5963b1e32555124320e026d
ykis-0-0 <64165725+ykis-0-0@users.noreply.github.com>
2021-06-16 17:15:50 +0800

    Say goodbye to our old files

--- src/prefs/general.ts
+++
@@ -1,7 +0,0 @@
-import type GeneralPrefSchema from '::/prefs_exts/general/schema.ts';
-
-let generalPrefsUser: GeneralPrefSchema = {
-  serverPort: 80,
-};
-
-export default generalPrefsUser;
--- src/prefs/logging.ts
+++
@@ -1,7 +0,0 @@
-import type LoggingPrefSchema from '::/logging/prefs/schema.ts';
-
-let loggingPrefsUser: LoggingPrefSchema = {
-  logRoot: './logs/',
-};
-
-export default loggingPrefsUser;
