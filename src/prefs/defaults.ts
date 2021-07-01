d4c5e538b858c3f4b980e13385fed0322e1a0448
ykis-0-0 <64165725+ykis-0-0@users.noreply.github.com>
2021-07-01 19:34:19 +0800

    Update `/pref/defaults.ts` to match the other side

--- src/prefs/defaults.ts
+++ src/prefs/defaults.ts
@@ -1,2 +1,12 @@
-export { defaults as General } from '::/prefs/general/defaults.ts';
-export { defaults as Logging } from '::/logging/prefs/defaults.ts';
+import type { TheSchema } from './schemata.ts';
+import type { SummarizedDefaultSchema } from '::/utils/typedefs/schemata.ts';
+
+import { defaults as general } from '::/prefs/general/defaults.ts';
+import { defaults as logging } from '::/logging/prefs/defaults.ts';
+
+const defaults: SummarizedDefaultSchema<TheSchema> = {
+  general,
+  logging,
+} as const;
+
+export default defaults;
