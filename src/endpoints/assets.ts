6e9a9df372b44536a67ba625874195e41914b884
ykis-0-0 <64165725+ykis-0-0@users.noreply.github.com>
2021-08-25 15:17:03 +0800

    To Add protection against malform request with escaped slash ("%2F")

--- src/endpoints/assets.ts
+++ src/endpoints/assets.ts
@@ -6,10 +6,14 @@ import { roots } from '::/utils/elevator.ts';
 const router = new Oak.Router();
 
 router.get('/:path(.+)', async function self(ctx, next) {
-  const paramsEncoded = await getEncodedParams(ctx, self);
-  await Oak.send(ctx, paramsEncoded['path'] as string, {
+  const paramsEncoded = await getEncodedParams(ctx, self, true);
+  if(/%2F/i.test(paramsEncoded['path'])) ctx.throw(Oak.Status.BadRequest, `Invalid character in request URL`);
+
+  const sendPromise = Oak.send(ctx, ctx.params['path'] as string, {
     root: roots.assetRoot,
-  })
+  });
+
+  await sendPromise;
 });
 
 export { router };
